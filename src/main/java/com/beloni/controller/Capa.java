a8890b1318d7c68cd0eceac80ec6808cea67e8d0
Caue <cbeloni@gmail.com>
2017-11-04 11:07:14 -0200

    Inclusão data

--- src/main/java/com/beloni/controller/Capa.java
+++ src/main/java/com/beloni/controller/Capa.java
@@ -5,15 +5,39 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Controller
 public class Capa {
 
     @RequestMapping(value="/")
-    public String start(Map<String, Object> model){
+    public String start(Map<String, Object> model, HttpSession session, HttpServletRequest request){
+        String data_casamento = "19 11 2017";
+        DateFormat dateFormat = new SimpleDateFormat("dd MM yyyy");
+        Long dias_faltantes = null;
+        try{
+            Date date2 = dateFormat.parse(data_casamento);
+            Date date = new Date();
+            long diff = date2.getTime() - date.getTime();
+            dias_faltantes = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+            System.out.println ("Days: " + TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS));
+            String teste = "Teste 2";
+            System.out.println(dateFormat.format(date));
+        }catch (ParseException e){
+            e.printStackTrace();
+
+        }
+
         System.out.println("Current dir:");
         System.out.println(System.getProperty("user.dir"));
+        request.setAttribute("dias_faltantes", dias_faltantes);
         return "capa";
 
     }
